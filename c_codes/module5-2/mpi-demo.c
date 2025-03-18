eb975a080bae162fc89175a477241aad42981af1
Valeria Barra <valeriabarra21@gmail.com>
2025-03-18 16:20:13 -0700

    Elaborate a bit more on MPI demo

--- c_codes/module5-2/mpi-demo.c
+++ c_codes/module5-2/mpi-demo.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
   int size, rank, sum;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Allreduce(&rank, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Allreduce(&rank, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD); // performs the sum of the first n-1 integers
   printf("I am rank %d of %d: sum=%d\n", rank, size, sum);
   return MPI_Finalize();
 }
